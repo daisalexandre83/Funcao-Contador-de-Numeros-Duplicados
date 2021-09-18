fe234afaff3a9b88bca5231895c2e0b0a7d28088
Daiane de Souza Alexandre <dsalexandre83@gmail.com>
2021-09-18 12:57:16 -0300

    Alteração na lógica da função em JS

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
+
+
 //Criar uma variável auxiliar para o index do array
 //Se essa variável tiver  mais que dois valores iguais, deve-se filtrar
 //mostrar a quantidade desses números repetidos.
 
 function countDuplicate(numbers) {
-    numbers =[1,2,3,4,1,5,6,7,2,8,9,3,3];
-    numbers[index] = value;
+   let value = numbers[index]; 
+   let duplicate = numbers.filter(value => value === value); //mostrar
+   let qt = duplicate;
+   if (qt > 1) {
+       
+   }
+  
 
+  console.log(duplicate);
 }
+
+let numbers =[1,2,3,4,1,5,6,7,2,8,9,3,3];
+
+countDuplicate(numbers);
