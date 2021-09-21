b4cd0477c3b25ced72359f01da1051668a583ac2
Daiane de Souza Alexandre <dsalexandre83@gmail.com>
2021-09-21 17:26:00 -0300

    Alterações na função do Javascript

--- index.js
+++ index.js
@@ -5,18 +5,14 @@
 //mostrar a quantidade desses números repetidos.
 
 function countDuplicate(numbers) {
-   
-   let duplicate1 = numbers.filter(index => index === 1); //mostrar se há valores iguais
+   let duplicate1 = numbers.filter(index => index === 1); 
    let duplicate2 = numbers.filter(index=> index ===2);
    let duplicate3 = numbers.filter(index=> index ===3);
-  /*  let qt = duplicate; 
-   if (qt > 1) { */
-    
-      
-  console.log(duplicate1.length);
-  console.log(duplicate2.length);
-  console.log(duplicate3.length);
-  
+
+   console.log(` A quantidade do número 1 é: ${duplicate1.length}`);
+   console.log(` A quantidade do número 2 é: ${duplicate2.length}`);
+   console.log(` A quantidade do número 3 é: ${duplicate3.length}`);
+   
 }
 
 let numbers =[1,2,3,4,1,5,6,7,2,8,9,3,3,1,4,5];
